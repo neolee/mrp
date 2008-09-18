40dbe0fbd14b08230407bc2414956bcfdefcba47
Neo Lee <neo.lee@gmail.com>
2008-09-18 17:52:17 +0800

    Add basic full-text searching using act_as_ferret.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,31 +9,36 @@ class SearchController < ApplicationController
     forum_id = params[:forum_id]
     start_date = Time.mktime(params[:start_date][:year], params[:start_date][:month], params[:start_date][:day])
     end_date = Time.mktime(params[:end_date][:year], params[:end_date][:month], params[:end_date][:day])
+    full_text = params[:full_text]
     
-    cond = []
-    if subject and not subject.empty?
-      cond << "subject regexp :subject"   # MySQL only
-    end
+    if full_text and not full_text.empty?
+      messages = Message.find_with_ferret(full_text).collect { |m| m.post_id }
+    else
+      cond = []
+      if subject and not subject.empty?
+        cond << "subject regexp :subject"   # MySQL only
+      end
     
-    if user_name and not user_name.empty?
-      user = User.find(:all, :conditions => ["user_name = ?", user_name], :order => "id").first
-      params[:user_id] = user.id
-      cond << "user_id = :user_id"
-    end
+      if user_name and not user_name.empty?
+        user = User.find(:all, :conditions => ["user_name = ?", user_name], :order => "id").first
+        params[:user_id] = user.id
+        cond << "user_id = :user_id"
+      end
 
-    if forum_id and not forum_id.empty? and forum_id.to_i > 0
-      params[:forum_id] = forum_id.to_i
-      cond << "forum_id = :forum_id"
-    end
+      if forum_id and not forum_id.empty? and forum_id.to_i > 0
+        params[:forum_id] = forum_id.to_i
+        cond << "forum_id = :forum_id"
+      end
     
-    if end_date and end_date < Time.now
-      params[:start_date] = make_db_time(start_date)
-      params[:end_date] = make_db_time(end_date)
-      cond << "creation_date between :start_date and :end_date"
-    end
+      if end_date and end_date < Time.now
+        params[:start_date] = make_db_time(start_date)
+        params[:end_date] = make_db_time(end_date)
+        cond << "creation_date between :start_date and :end_date"
+      end
 
-    messages = Message.find(:all, :select => "distinct post_id", 
-      :conditions => [cond.join(" and "), params]).collect { |m| m.post_id }
+      messages = Message.find(:all, :select => "distinct post_id", 
+        :conditions => [cond.join(" and "), params]).collect { |m| m.post_id }
+    end
     
     @results = Post.find(:all, :conditions => ["id in (?)", messages], :order => "creation_date"
       ).paginate :page => params[:page], :per_page => posts_per_page
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :forum
   belongs_to :post
+  
+  acts_as_ferret
 end
