4edea49bf9a61d02f986d19d7e8aee63146528d7
Neo Lee <neo.lee@gmail.com>
2011-01-03 18:51:53 +0800

    Modified environment.rb to keep MRP running on a multi-version Rails environment.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -68,5 +68,7 @@ end
 
 require 'will_paginate'
 
+gem 'acts_as_ferret', '=0.4.4'
 require 'acts_as_ferret'
+
 require 'rmmseg/ferret'
